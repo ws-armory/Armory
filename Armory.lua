fca7b1004881cc1ecefa1fc62ed3997f961bc928
Luc Sarzyniec <luc.sarzyniec@gmail.com>
2014-11-20 19:03:58 +0100

    Initial import

---
+++ Armory.lua
@@ -0,0 +1,122 @@
+-----------------------------------------------------------------------------------------------
+-- Client Lua Script for Armory
+-- Copyright (c) NCsoft. All rights reserved
+-----------------------------------------------------------------------------------------------
+ 
+require "Window"
+ 
+-----------------------------------------------------------------------------------------------
+-- Armory Module Definition
+-----------------------------------------------------------------------------------------------
+local Armory = {} 
+ 
+-----------------------------------------------------------------------------------------------
+-- Constants
+-----------------------------------------------------------------------------------------------
+-- e.g. local kiExampleVariableMax = 999
+ 
+-----------------------------------------------------------------------------------------------
+-- Initialization
+-----------------------------------------------------------------------------------------------
+function Armory:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self 
+
+    -- initialize variables here
+
+    return o
+end
+
+function Armory:Init()
+	local bHasConfigureFunction = false
+	local strConfigureButtonText = ""
+	local tDependencies = {
+		-- "UnitOrPackageName",
+	}
+    Apollo.RegisterAddon(self, bHasConfigureFunction, strConfigureButtonText, tDependencies)
+end
+ 
+
+-----------------------------------------------------------------------------------------------
+-- Armory OnLoad
+-----------------------------------------------------------------------------------------------
+function Armory:OnLoad()
+    -- load our form file
+	self.xmlDoc = XmlDoc.CreateFromFile("Armory.xml")
+	self.xmlDoc:RegisterCallback("OnDocLoaded", self)
+end
+
+-----------------------------------------------------------------------------------------------
+-- Armory OnDocLoaded
+-----------------------------------------------------------------------------------------------
+function Armory:OnDocLoaded()
+
+	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
+	    self.wndMain = Apollo.LoadForm(self.xmlDoc, "ArmoryForm", nil, self)
+		if self.wndMain == nil then
+			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
+			return
+		end
+		
+	    self.wndMain:Show(false, true)
+
+		-- if the xmlDoc is no longer needed, you should set it to nil
+		-- self.xmlDoc = nil
+		
+		-- Register handlers for events, slash commands and timer, etc.
+		-- e.g. Apollo.RegisterEventHandler("KeyDown", "OnKeyDown", self)
+		Apollo.RegisterSlashCommand("armory", "OnArmoryOn", self)
+		
+		-- Do additional Addon initialization here
+	end
+end
+
+-----------------------------------------------------------------------------------------------
+-- Armory Functions
+-----------------------------------------------------------------------------------------------
+-- Define general functions here
+
+-- on SlashCommand "/armory"
+function Armory:OnArmoryOn()
+	local JSON = Apollo.GetPackage("Lib:dkJSON-2.5").tPackage
+	local items = {}
+
+	for key, item in ipairs(GameLib.GetPlayerUnit():GetEquippedItems()) do
+		items[tostring(key)] = item:GetItemId()
+	end
+	
+	local data = Armory:Base64(JSON.encode(items))
+
+	self.wndMain:Invoke() -- show the window
+	self.wndMain:FindChild("Text"):SetText(data)
+	self.wndMain:FindChild("CopyButton"):SetActionData(GameLib.CodeEnumConfirmButtonType.CopyToClipboard,data)
+	self.wndMain:Show(true)
+end
+
+-- when the Close button is clicked
+function Armory:OnClose()
+	self.wndMain:Close() -- hide the window
+end
+
+-- Base64 encode
+-- This code snippet comes from http://lua-users.org/wiki/BaseSixtyFour
+function Armory:Base64(data)
+	local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+    return ((data:gsub('.', function(x) 
+        local r,b='',x:byte()
+        for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+        return r;
+    end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+        if (#x < 6) then return '' end
+        local c=0
+        for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+        return b:sub(c+1,c+1)
+    end)..({ '', '==', '=' })[#data%3+1])
+end
+
+-----------------------------------------------------------------------------------------------
+-- Armory Instance
+-----------------------------------------------------------------------------------------------
+local ArmoryInst = Armory:new()
+ArmoryInst:Init()
